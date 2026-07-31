15a6981d453e6d8798648bb32835279ba580c987
maguibaires-netizen <maguybaires@gmail.com>
2026-07-31 20:13:41 +0000

    Completar IDs de Sheets y agrandar el embed

--- src/pages/PendientesConciliar.jsx
+++ src/pages/PendientesConciliar.jsx
@@ -1,7 +1,7 @@
 import SheetEmbed from "../components/SheetEmbed.jsx";
 
 // Reemplazá PEGAR_ID_ACA por el ID real del Sheet (mismo procedimiento de siempre).
-const SHEET_ID = "PEGAR_ID_ACA";
+const SHEET_ID = "1J7gdCPIQqZq2VYHxRXTgPQfxneAxmwHpJ78Ny0uqpQU";
 
 export default function PendientesConciliar() {
   return (
