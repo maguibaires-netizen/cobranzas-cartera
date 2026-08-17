f73e70bc5ee0cb06443b6d09f59d8e8443251887
maguibaires-netizen <maguybaires@gmail.com>
2026-08-17 14:50:46 +0000

    Arreglar salto de scroll en planillas embebidas + módulo de retenciones

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,15 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 
+const FULL_HEIGHT_PATHS = ["/pedidos-pendientes", "/cheques-rechazados", "/pendientes-conciliar"];
+
 export default function Layout({ children }) {
+  const location = useLocation();
+  const fullHeight = FULL_HEIGHT_PATHS.includes(location.pathname);
+
   return (
-    <div className="shell">
-      <div className="screen">
+    <div className={`shell${fullHeight ? " shell-fixed" : ""}`}>
+      <div className={`screen${fullHeight ? " screen-fixed" : ""}`}>
         <Navbar />
         {children}
       </div>
